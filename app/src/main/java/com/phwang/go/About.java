97128ebd754b368212b5fd8c5278ed5e6568ec06
paulhwang <phwang@gmail.com>
2021-03-20 16:28:59 +0800

    about

--- app/src/main/java/com/phwang/go/About.java
+++ app/src/main/java/com/phwang/go/About.java
@@ -1,4 +1,14 @@
 package com.phwang.go;
 
-public class About {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstancesState) {
+       super.onCreate(savedInstancesState);
+        Log.d("+++++eeeGo+++++", "About");
+       setContentView(R.layout.about);
+    }
 }
--- app/src/main/java/com/phwang/go/MainActivity.java
+++ app/src/main/java/com/phwang/go/MainActivity.java
@@ -2,15 +2,54 @@ package com.phwang.go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("+++++Go+++++", "MainActivity");
+
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newGameButton = findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.continue_button:
+                Log.d("+++++aaaGo+++++", "MainActivity");
+                i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            case R.id.new_game_button:
+                Log.d("+++++bbbGo+++++", "MainActivity");
+                i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            case R.id.about_button:
+                Log.d("+++++cccGo+++++", "MainActivity");
+                i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+            case R.id.exit_button:
+                Log.d("+++++dddGo+++++", "MainActivity");
+                i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+        }
     }
 }
