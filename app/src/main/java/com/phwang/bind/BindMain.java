5e627943645920122d07746c481ceb657f7152d8
paulhwang <phwang@gmail.com>
2021-03-21 23:05:05 +0800

    bindmain

--- app/src/main/java/com/phwang/bind/BindMain.java
+++ app/src/main/java/com/phwang/bind/BindMain.java
@@ -5,14 +5,7 @@
  *
  ******************************************************************************
  */
-
 package com.phwang.bind;
 
-import com.phwang.core.utils.AbendInterface;
-import android.util.Log;
-
-public class MainAbend implements AbendInterface {
-    public void log(String str_val) {
-        Log.e("eee", str_val);
-    }
+public class BindMain {
 }
