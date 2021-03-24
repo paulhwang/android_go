c841579d4171023334e2d97e5aabcb258abe6dd7
paul.hwang (黃文樸 - Getac) <phwang@gmail.com>
2021-03-24 18:09:06 +0800

    main

--- app/src/main/java/com/phwang/go/bind/BindReceiver.java
+++ app/src/main/java/com/phwang/go/bind/BindReceiver.java
@@ -36,14 +36,13 @@ public class BindReceiver extends BroadcastReceiver {
     public void onReceive(Context context_val, Intent intent_val) {
         Bundle bundle = intent_val.getExtras();
         String stamp = bundle.getString(BundleIndexDefine.STAMP);
-        String command_or_response = bundle.getString(BundleIndexDefine.COMMAND_OR_RESPONSE);
-        Log.e(TAG, "onReceive() stamp=" + stamp);
-        Log.e(TAG, "onReceive() from=" + bundle.getString(BundleIndexDefine.FROM));
         if ((stamp == null) || !stamp.equals(BundleIndexDefine.THE_STAMP)) {
             Log.e(TAG, "onReceive() bad-stamp. command=" + bundle.getString(BundleIndexDefine.COMMAND));
             return;
         }
 
+        //Log.e(TAG, "onReceive() from=" + bundle.getString(BundleIndexDefine.FROM));
+        String command_or_response = bundle.getString(BundleIndexDefine.COMMAND_OR_RESPONSE);
         if (command_or_response.equals(BundleIndexDefine.IS_COMMAND)) {
             this.handleCommand(bundle);
         }
