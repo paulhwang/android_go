8b83acf2971f27256aca4194ae93fab0a5b668f2
paulhwang <phwang@gmail.com>
2021-03-27 00:29:04 +0800

    encodets

--- app/src/main/java/com/phwang/core/utils/Encoders.java
+++ app/src/main/java/com/phwang/core/utils/Encoders.java
@@ -21,30 +21,12 @@ public class Encoders {
         buf.append(str);
         return buf.toString();
     }
-
-    public static String iEncodeRaw1(int number_val) {
-        return iEncodeRaw(number_val, 1);
-    }
-
-    public static String iEncodeRaw2(int number_val) {
-        return iEncodeRaw(number_val, 2);
-    }
-
-    public static String iEncodeRaw3(int number_val) {
-        return iEncodeRaw(number_val, 3);
-    }
-
-    public static String iEncodeRaw4(int number_val) {
-        return iEncodeRaw(number_val, 4);
-    }
-
-    public static String iEncodeRaw5(int number_val) {
-        return iEncodeRaw(number_val, 5);
-    }
-
-    public static String iEncodeRaw6(int number_val) {
-        return iEncodeRaw(number_val, 6);
-    }
+    public static String iEncodeRaw1(int number_val) { return iEncodeRaw(number_val, 1); }
+    public static String iEncodeRaw2(int number_val) { return iEncodeRaw(number_val, 2); }
+    public static String iEncodeRaw3(int number_val) { return iEncodeRaw(number_val, 3); }
+    public static String iEncodeRaw4(int number_val) { return iEncodeRaw(number_val, 4); }
+    public static String iEncodeRaw5(int number_val) { return iEncodeRaw(number_val, 5); }
+    public static String iEncodeRaw6(int number_val) { return iEncodeRaw(number_val, 6); }
 
     public static String iEncode111(int number_val, int size_val) {
         String str = Integer.toString(number_val);
@@ -61,30 +43,12 @@ public class Encoders {
         String str = iEncodeRaw(number_val, size_val);
         return sEncode2(str);
     }
-
-    public static String iEncodeLen1(int number_val) {
-        return iEncodeLen(number_val, 1);
-    }
-
-    public static String iEncodeLen2(int number_val) {
-        return iEncodeLen(number_val, 2);
-    }
-
-    public static String iEncodeLen3(int number_val) {
-        return iEncodeLen(number_val, 3);
-    }
-
-    public static String iEncodeLen4(int number_val) {
-        return iEncodeLen(number_val, 4);
-    }
-
-    public static String iEncodeLen5(int number_val) {
-        return iEncodeLen(number_val, 5);
-    }
-
-    public static String iEncodeLen6(int number_val) {
-        return iEncodeLen(number_val, 6);
-    }
+    public static String iEncodeLen1(int number_val) { return iEncodeLen(number_val, 1); }
+    public static String iEncodeLen2(int number_val) { return iEncodeLen(number_val, 2); }
+    public static String iEncodeLen3(int number_val) { return iEncodeLen(number_val, 3); }
+    public static String iEncodeLen4(int number_val) { return iEncodeLen(number_val, 4); }
+    public static String iEncodeLen5(int number_val) { return iEncodeLen(number_val, 5); }
+    public static String iEncodeLen6(int number_val) { return iEncodeLen(number_val, 6); }
 
     public static int iDecodeRaw(String str_val) {
         int str_len = str_val.length();
@@ -123,80 +87,37 @@ public class Encoders {
         buf.append(str_val);
         return buf.toString();
     }
-
-    public static String sEncode2(String str_val) {
-        return sEncode(str_val, 2);
-    }
-
-    public static String sEncode5(String str_val) {
-        return sEncode(str_val, 5);
-    }
+    public static String sEncode2(String str_val) { return sEncode(str_val, 2); }
+    public static String sEncode5(String str_val) { return sEncode(str_val, 5); }
 
     public static String sDecode(String str_val, int size_val) {
         int len = Encoders.iDecodeRaw(str_val.substring(0, size_val));
         return str_val.substring(size_val, size_val + len);
     }
-
-    public static String sDecode1(String str_val) {
-        return sDecode(str_val, 1);
-    }
-
-    public static String sDecode2(String str_val) {
-        return sDecode(str_val, 2);
-    }
-
-    public static String sDecode5(String str_val) {
-        return sDecode(str_val, 5);
-    }
+    public static String sDecode1(String str_val) { return sDecode(str_val, 1); }
+    public static String sDecode2(String str_val) { return sDecode(str_val, 2); }
+    public static String sDecode5(String str_val) { return sDecode(str_val, 5); }
 
     public static String sDecode_(String str_val, int size_val) {
         return str_val.substring(size_val + Encoders.iDecodeRaw(str_val.substring(0, size_val)));
     }
-
-    public static String sDecode1_(String str_val) {
-        return sDecode_(str_val, 1);
-    }
-
-    public static String sDecode2_(String str_val) {
-        return sDecode_(str_val, 2);
-    }
-
-    public static String sDecode5_(String str_val) {
-        return sDecode_(str_val, 5);
-    }
+    public static String sDecode1_(String str_val) { return sDecode_(str_val, 1); }
+    public static String sDecode2_(String str_val) { return sDecode_(str_val, 2); }
+    public static String sDecode5_(String str_val) { return sDecode_(str_val, 5); }
 
     public static String sSubstring(String str_val, int size_val) {
         int len = Encoders.iDecodeRaw(str_val.substring(0, size_val));
         return str_val.substring(0, size_val + len);
     }
-
-    public static String sSubstring1(String str_val) {
-        return sSubstring(str_val, 1);
-    }
-
-    public static String sSubstring2(String str_val) {
-        return sSubstring(str_val, 2);
-    }
-
-    public static String sSubstring5(String str_val) {
-        return sSubstring(str_val, 5);
-    }
+    public static String sSubstring1(String str_val) { return sSubstring(str_val, 1); }
+    public static String sSubstring2(String str_val) { return sSubstring(str_val, 2); }
+    public static String sSubstring5(String str_val) { return sSubstring(str_val, 5); }
 
 
     public static String sSubstring_(String str_val, int size_val) {
         return str_val.substring(size_val + Encoders.iDecodeRaw(str_val.substring(0, size_val)));
     }
-
-    public static String sSubstring1_(String str_val) {
-        return sSubstring_(str_val, 1);
-    }
-
-    public static String sSubstring2_(String str_val) {
-        return sSubstring_(str_val, 2);
-    }
-
-    public static String sSubstring5_(String str_val) {
-        return sSubstring_(str_val, 5);
-    }
-
+    public static String sSubstring1_(String str_val) { return sSubstring_(str_val, 1); }
+    public static String sSubstring2_(String str_val) { return sSubstring_(str_val, 2); }
+    public static String sSubstring5_(String str_val) { return sSubstring_(str_val, 5); }
 }
