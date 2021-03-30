60be39b794a90affbdf543b99bbc1cfa99f19fe2
paulhwang <phwang@gmail.com>
2021-03-30 10:31:25 +0800

    result

---
+++ app/src/main/java/com/phwang/client/ClientFabricResultImport.java
@@ -0,0 +1,17 @@
+/*
+ ******************************************************************************
+ *
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package com.phwang.client;
+
+import com.phwang.core.fabric.FabricExport;
+
+public class ClientFabricResultImport {
+    public static final char FABRIC_RESULT_SUCCEED = FabricExport.FABRIC_RESULT_SUCCEED;
+    public static final char FABRIC_RESULT_ACCOUNT_NOT_EXIST = FabricExport.FABRIC_RESULT_ACCOUNT_NOT_EXIST;
+    public static final char FABRIC_RESULT_PASSWORD_NOT_GOOD = FabricExport.FABRIC_RESULT_PASSWORD_NOT_GOOD;
+}
---
+++ app/src/main/java/com/phwang/core/fabric/FabricResultExport.java
@@ -0,0 +1,15 @@
+/*
+ ******************************************************************************
+ *
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package com.phwang.core.fabric;
+
+public class FabricResultExport {
+    public static final char FABRIC_RESULT_SUCCEED = '0';
+    public static final char FABRIC_RESULT_ACCOUNT_NOT_EXIST = '1';
+    public static final char FABRIC_RESULT_PASSWORD_NOT_GOOD = '2';
+}
