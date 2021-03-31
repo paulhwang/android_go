03c978d57eefa90be029a896b5481b82eb94578b
paulhwang <phwang@gmail.com>
2021-03-31 23:07:58 +0800

    commands

--- app/src/main/java/com/phwang/core/fabric/FabricExport.java
+++ app/src/main/java/com/phwang/core/fabric/FabricExport.java
@@ -26,5 +26,4 @@ public class FabricExport {
     public static final char FABRIC_THEME_RESPOND_SETUP_ROOM = 'r';
     public static final char FABRIC_THEME_COMMAND_PUT_ROOM_DATA = 'D';
     public static final char FABRIC_THEME_RESPOND_PUT_ROOM_DATA = 'd';
-
 }
