7d633d6373ea4cc13fbcecc94d3efcabbfd61620
paulhwang <phwang@gmail.com>
2021-04-04 04:27:30 +0800

    encode

---
+++ app/src/main/java/com/phwang/core/utils/fabric/FabricDecode.java
@@ -0,0 +1,48 @@
+/*
+ ******************************************************************************
+ *
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package com.phwang.core.utils.fabric;
+
+import com.phwang.core.utils.encoders.Encoders;
+
+public class FabricDecode {
+    private static final String TAG = "FabricDecode";
+    private static final int MAX_ARRAY_SIZE = 10;
+
+    private char command_;
+    private char result_;
+    private String linkIdStr_;
+    private String sessionIdStr_;
+    private String[] strings_ = new String[MAX_ARRAY_SIZE];
+
+    public char command() { return this.command_; };
+    public char result() { return this.result_; };
+    public String linkIdStr() { return this.linkIdStr_; };
+    public String sessionIdStr() { return this.sessionIdStr_; };
+    public String strings(int index_val) { return this.strings_[index_val]; };
+
+    public FabricDecode(String fabric_data_str_val) {
+        this.command_ = fabric_data_str_val.charAt(0);
+        this.result_ = fabric_data_str_val.charAt(1);
+        String rest_str = Encoders.sDecode5(fabric_data_str_val.substring(2));
+
+        this.linkIdStr_ = Encoders.sSubstring2(rest_str);
+        rest_str = Encoders.sSubstring2_(rest_str);
+
+        this.sessionIdStr_ = Encoders.sSubstring2(rest_str);
+        rest_str = Encoders.sSubstring2_(rest_str);
+
+        int count = rest_str.charAt(0) - 48;
+        rest_str = rest_str.substring(1);
+
+        for (int i = 0; i < count; i ++) {
+            this.strings_[i] = Encoders.sSubstring2(rest_str);
+            rest_str = Encoders.sSubstring2_(rest_str);
+        }
+    }
+}
---
+++ app/src/main/java/com/phwang/core/utils/fabric/FabricEncode.java
@@ -0,0 +1,47 @@
+/*
+ ******************************************************************************
+ *
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package com.phwang.core.utils.fabric;
+
+import com.phwang.core.utils.encoders.Encoders;
+
+public class FabricEncode {
+    private static final String TAG = "FabricEncode";
+    private static final int MAX_ARRAY_SIZE = 10;
+
+    private char command_;
+    private char result_;
+    private String linkIdStr_;
+    private String sessionIdStr_;
+    private int stringsCount_;
+    private String[] strings_ = new String[MAX_ARRAY_SIZE];
+
+    public void setStrings(int index_val, String string_val) { this.strings_[index_val] = string_val; }
+
+    public FabricEncode(char command_val, char result_val, String link_id_str_val, String session_id_str_val, int items_count_val) {
+        this.command_ = command_val;
+        this.result_ = result_val;
+        this.linkIdStr_ = link_id_str_val;
+        this.sessionIdStr_ = session_id_str_val;
+        this.stringsCount_ = items_count_val;
+    }
+
+    public String getEncodedString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append(this.command_);
+        buf.append(this.result_);
+        buf.append(this.linkIdStr_);
+        buf.append(this.sessionIdStr_);
+        buf.append(Encoders.iEncodeRaw1(this.stringsCount_));
+
+        for (int i = 0; i < this.stringsCount_; i++) {
+            buf.append(this.strings_[i]);
+        }
+        return buf.toString();
+    }
+}
