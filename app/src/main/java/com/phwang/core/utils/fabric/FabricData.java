9e9ea29531994797382a127ca1e5cecd99030366
paulhwang <phwang@gmail.com>
2021-04-05 08:37:45 +0800

    fabicdata

--- app/src/main/java/com/phwang/core/utils/fabric/FabricData.java
+++ app/src/main/java/com/phwang/core/utils/fabric/FabricData.java
@@ -23,6 +23,17 @@ public class FabricData {
     private int stringsCount_;
     private String[] stringList_ = new String[MAX_ARRAY_SIZE];
 
+    public char command() { return this.command_; };
+    public char result() { return this.result_; };
+    public char clientType() { return this.clientType_; }
+    public char theme() { return this.theme_; };
+    public String linkIdStr() { return this.linkIdStr_; };
+    public String sessionIdStr() { return this.sessionIdStr_; };
+    public String stringList(int index_val) { return this.stringList_[index_val]; };
+
+    public void setResult(char result_val) { this.result_ = result_val; }
+    public void setLinkIdStr(String link_id_str_val) { this.linkIdStr_ = link_id_str_val; }
+    public void setSessionIdStr(String session_id_str_val) { this.sessionIdStr_ = session_id_str_val; }
     public void setStringList(int index_val, String string_val) { this.stringList_[index_val] = string_val; }
 
     public FabricData(char command_val, char result_val, char client_type_val, char theme_val, String link_id_str_val, String session_id_str_val, int items_count_val) {
@@ -33,6 +44,29 @@ public class FabricData {
         this.linkIdStr_ = link_id_str_val;
         this.sessionIdStr_ = session_id_str_val;
         this.stringsCount_ = items_count_val;
+    }
+
+    public FabricData(String fabric_data_str_val) {
+        String rest_str = fabric_data_str_val;
+        this.command_ = rest_str.charAt(0);
+        this.result_ = rest_str.charAt(1);
+        this.clientType_ = rest_str.charAt(2);
+        this.theme_ = rest_str.charAt(3);
+        rest_str = rest_str.substring((4));
+
+        this.linkIdStr_ = Encoders.sSubstring2(rest_str);
+        rest_str = Encoders.sSubstring2_(rest_str);
+
+        this.sessionIdStr_ = Encoders.sSubstring2(rest_str);
+        rest_str = Encoders.sSubstring2_(rest_str);
+
+        int count = rest_str.charAt(0) - 48;
+        rest_str = rest_str.substring(1);
+
+        for (int i = 0; i < count; i ++) {
+            this.stringList_[i] = Encoders.sDecode6(rest_str);
+            rest_str = Encoders.sSubstring6_(rest_str);
+        }
     }
 
     public String getEncodedString() {
