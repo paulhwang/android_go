f870b536059e1c078e3c5c033b923265be0a90ba
paulhwang <phwang@gmail.com>
2021-04-06 17:08:06 +0800

    protocols

--- app/src/main/java/com/phwang/core/protocols/engine/EngineDataStr.java
+++ app/src/main/java/com/phwang/core/protocols/engine/EngineDataStr.java
@@ -9,11 +9,6 @@ public class EngineDataStr {
     public static char   getResult(String engine_data_str_val)        { return engine_data_str_val.charAt(EngineData.RESULT_INDEX); }
     public static String getResultStr(String engine_data_str_val)     { return engine_data_str_val.substring(EngineData.RESULT_INDEX, EngineData.RESULT_INDEX + 1); }
 
-    public static char   getClientType(String engine_data_str_val)    { return engine_data_str_val.charAt(EngineData.CLIENT_TYPE_INDEX); }
-    public static String getClientTypeStr(String engine_data_str_val) { return engine_data_str_val.substring(EngineData.CLIENT_TYPE_INDEX, EngineData.CLIENT_TYPE_INDEX + 1); }
-
     public static char   getTheme(String engine_data_str_val)         { return engine_data_str_val.charAt(EngineData.THEME_INDEX); }
     public static String getThemeStr(String engine_data_str_val)      { return engine_data_str_val.substring(EngineData.THEME_INDEX, EngineData.THEME_INDEX + 1); }
-
-    public static String getJobIdStr(String engine_data_str_val)      { return Encoders.sSubstring2(engine_data_str_val.substring(EngineData.JOB_ID_STR_INDEX)); }
 }
--- app/src/main/java/com/phwang/core/protocols/theme/ThemeDataStr.java
+++ app/src/main/java/com/phwang/core/protocols/theme/ThemeDataStr.java
@@ -9,11 +9,6 @@ public class ThemeDataStr {
     public static char   getResult(String theme_data_str_val)        { return theme_data_str_val.charAt(ThemeData.RESULT_INDEX); }
     public static String getResultStr(String theme_data_str_val)     { return theme_data_str_val.substring(ThemeData.RESULT_INDEX, ThemeData.RESULT_INDEX + 1); }
 
-    public static char   getClientType(String theme_data_str_val)    { return theme_data_str_val.charAt(ThemeData.CLIENT_TYPE_INDEX); }
-    public static String getClientTypeStr(String theme_data_str_val) { return theme_data_str_val.substring(ThemeData.CLIENT_TYPE_INDEX, ThemeData.CLIENT_TYPE_INDEX + 1); }
-
     public static char   getTheme(String theme_data_str_val)         { return theme_data_str_val.charAt(ThemeData.THEME_INDEX); }
     public static String getThemeStr(String theme_data_str_val)      { return theme_data_str_val.substring(ThemeData.THEME_INDEX, ThemeData.THEME_INDEX + 1); }
-
-    public static String getJobIdStr(String theme_data_str_val)      { return Encoders.sSubstring2(theme_data_str_val.substring(ThemeData.JOB_ID_STR_INDEX)); }
 }
