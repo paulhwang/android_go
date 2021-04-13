3e798af623d7feac03914bce908c428613fd651e
paulhwang <phwang@gmail.com>
2021-04-14 00:41:06 +0800

    config

--- app/src/main/java/com/phwang/go/go/game/GoGameBoard.java
+++ app/src/main/java/com/phwang/go/go/game/GoGameBoard.java
@@ -9,6 +9,8 @@
 package com.phwang.go.go.game;
 
 import android.util.Log;
+
+import com.phwang.core.go.GoConfigInfo;
 import com.phwang.core.go.GoDefine;
 import com.phwang.core.go.GoMoveInfo;
 import com.phwang.core.utils.encoders.Encoders;
@@ -17,38 +19,22 @@ public class GoGameBoard {
     private static final String TAG = "GoGameBoard";
 
     private GoGameActivity goGame_;
-    private int boardSize_ = 19;
     private final int[][] boardArray_ = new int[20][20];
     private int totalMoves_ = 0;
     private int nextColor_ = 1;
-    private int myColor_;
+    private GoConfigInfo goConfigInfo_;
 
-    protected int myColor() { return this.myColor_; };
-    protected int boardSize() { return this.boardSize_; }
-    protected void setBoardSize(int val) { this.boardSize_ = val; }
+    protected int myColor() { return this.goConfigInfo_.color(); };
+    protected int boardSize() { return this.goConfigInfo_.boardSize(); }
     protected int board(int x_val, int y_val) { return this.boardArray_[x_val][y_val]; }
     protected void setBoard(int x_val, int y_val, int val) { this.boardArray_[x_val][y_val] = val; }
     protected int totalMoves() { return this.totalMoves_; };
     protected int nextColor() { return this.nextColor_; };
     protected Boolean isValidMove(int x_val, int y_val) { return (this.boardArray_[x_val][y_val] == 0) ? true : false; }
 
-    public GoGameBoard(GoGameActivity go_game_val, String config_str_val) {
+    public GoGameBoard(GoGameActivity go_game_val, String go_config_info_str_val) {
         this.goGame_ = go_game_val;
-        this.decodeConfig(config_str_val);
-    }
-
-    void decodeConfig(String config_str_val) {
-        //Log.e(TAG, "decodeConfig() config_str=" + config_str_val);
-
-        String board_size_str = config_str_val.substring(1, 3);
-        //String handicap_str = config_str.substring(3, 5);
-        //String komi_str = config_str.substring(5, 7);
-        String color_str = config_str_val.substring(7, 8);
-
-        this.boardSize_ = Encoders.iDecodeRaw(board_size_str);
-        //this.handicapPoint_ = Encoders.iDecodeRaw(handicap_str);
-        //this.komiPoint_ = Encoders.iDecodeRaw(komi_str);
-        this.myColor_ = Encoders.iDecodeRaw(color_str);
+        this.goConfigInfo_ = new GoConfigInfo(go_config_info_str_val);
     }
 
     protected void decodeBoard(String data_str_val) {
@@ -56,9 +42,9 @@ public class GoGameBoard {
         this.totalMoves_ = Encoders.iDecodeRaw(total_moves_str);
         this.nextColor_ = data_str_val.charAt(3) - 48;
         String rest_str = data_str_val.substring(4);
-        for (int i = 0; i < this.boardSize_; i++) {
-            for (int j = 0; j < this.boardSize_; j++) {
-                this.boardArray_[i][j] = rest_str.charAt(i * this.boardSize_ + j) - 48;
+        for (int i = 0; i < this.boardSize(); i++) {
+            for (int j = 0; j < this.boardSize(); j++) {
+                this.boardArray_[i][j] = rest_str.charAt(i * this.boardSize() + j) - 48;
             }
         }
     }
