7a3953db53b97d7b770bb5cbc870d97c9ee9f149
paulhwang <phwang@gmail.com>
2021-04-18 10:25:01 +0800

    color

--- app/src/main/java/com/phwang/core/go/GoConfigInfo.java
+++ app/src/main/java/com/phwang/core/go/GoConfigInfo.java
@@ -15,34 +15,34 @@ public class GoConfigInfo {
     private String objectName() {return "GoConfigInfo";}
 
     private int boardSize_;
+    private int myColor_;
     private int handicapPoint_;
     private int komiPoint_;
-    private int myColor_;
 
     public int boardSize() { return this.boardSize_; }
+    public int myColor() { return this.myColor_; }
     public int handicapPoint() { return this.handicapPoint_; }
     public int komiPoint() { return this.komiPoint_; }
-    public int myColor() { return this.myColor_; }
 
-    public GoConfigInfo(int board_size_val, int handicap_val, int komi_val, int color_val) {
+    public GoConfigInfo(int board_size_val, int my_color_val, int handicap_val, int komi_val) {
         this.boardSize_ = board_size_val;
+        this.myColor_ = my_color_val;
         this.handicapPoint_ = handicap_val;
         this.komiPoint_ = komi_val;
-        this.myColor_ = color_val;
     }
 
     public GoConfigInfo(String go_config_info_str_val) {
         this.debug(true, "GoConfigInfo", "go_config_info_str_val=" + go_config_info_str_val);
 
         String board_size_str = go_config_info_str_val.substring(1, 3);
-        String handicap_str   = go_config_info_str_val.substring(3, 5);
-        String komi_str       = go_config_info_str_val.substring(5, 7);
-        String color_str      = go_config_info_str_val.substring(7, 8);
+        String color_str      = go_config_info_str_val.substring(3, 4);
+        String handicap_str   = go_config_info_str_val.substring(4, 6);
+        String komi_str       = go_config_info_str_val.substring(6, 8);
 
         this.boardSize_     = Encoders.iDecodeRaw(board_size_str);
+        this.myColor_       = Encoders.iDecodeRaw(color_str);
         this.handicapPoint_ = Encoders.iDecodeRaw(handicap_str);
         this.komiPoint_     = Encoders.iDecodeRaw(komi_str);
-        this.myColor_       = Encoders.iDecodeRaw(color_str);
 
         this.debug(true, "GoConfigInfo", "boardSize=" + this.boardSize_ + " myColor_=" + this.myColor_);
     }
@@ -51,14 +51,14 @@ public class GoConfigInfo {
         StringBuilder buf = new StringBuilder();
         buf.append('G');
         buf.append(Encoders.iEncodeRaw2(this.boardSize_));
+        buf.append(Encoders.iEncodeRaw1(this.myColor_));
         buf.append(Encoders.iEncodeRaw2(this.handicapPoint_));
         buf.append(Encoders.iEncodeRaw2(this.komiPoint_));
-        buf.append(Encoders.iEncodeRaw1(this.myColor_));
         return buf.toString();
      }
 
-    public static String encodeConfig(int board_size_val, int handicap_val, int komi_val, int color_val) {
-        GoConfigInfo go_config_info = new GoConfigInfo(board_size_val, handicap_val, komi_val, color_val);
+    public static String encodeConfig(int board_size_val, int handicap_val, int komi_val, int my_color_val) {
+        GoConfigInfo go_config_info = new GoConfigInfo(board_size_val, my_color_val, handicap_val, komi_val);
         return go_config_info.encode();
     }
 
