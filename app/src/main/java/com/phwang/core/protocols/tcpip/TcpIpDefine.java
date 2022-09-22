9ebdeb8991507bae46d5867b739117a3e96adcbb
Paul Hwang <phwang@gmail.com>
2022-09-21 20:07:59 -0700

    x

--- app/src/main/java/com/phwang/core/protocols/tcpip/TcpIpDefine.java
+++ app/src/main/java/com/phwang/core/protocols/tcpip/TcpIpDefine.java
@@ -13,7 +13,7 @@ public class TcpIpDefine {
     public static final String FABRIC_SERVER_LOCAL_IP_ADDRESS_FOR_ANDROID = "127.0.0.1";
     public static final String THEME_SERVER_IP_ADDRESS_FOR_ENGINE = "127.0.0.1";
 
-    public static final short FABRIC_ANDROID_PORT = 8001;
+    public static final short FABRIC_ANDROID_PORT = 8006;
     public static final short FABRIC_FRONT_PORT   = 8001;
     public static final short FABRIC_THEME_PORT   = 8009;
     public static final short THEME_ENGINE_PORT   = 8005;
